26bd732ecf87d2c68048f1af5f904f3193703401
Pierre Thierry <pierre@nothos.net>
2012-10-08 03:07:53 +0200

    Import newly created rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NinjaRuby::Application.config.secret_token = '3aa0c2157b9020868df2ea505af71451ef239893177a4ed703d80e9dcaa9f460d2436c55d18687d335723d8fc1bef90e871bb46226e291096d360ee30ff3074d'
